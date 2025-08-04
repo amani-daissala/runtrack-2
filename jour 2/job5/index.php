e9e46a29eb57f64f4b5b626e588f603611c86496
amani-daissala <amani.daissala@laplateforme.io>
2025-08-04 16:13:06 +0200

    Add files via upload

---
+++ jour 2/job5/index.php
@@ -0,0 +1,19 @@
+<?php
+// Parcourir les nombres de 2 à 1000
+for ($i = 2; $i <= 1000; $i++) {
+    $estPremier = true; // On suppose que le nombre est premier
+
+    // Tester la divisibilité par tous les nombres de 2 à la racine carrée de $i
+    for ($j = 2; $j <= sqrt($i); $j++) {
+        if ($i % $j == 0) {
+            $estPremier = false; // Trouvé un diviseur → pas premier
+            break; // On arrête la boucle
+        }
+    }
+
+    // Si $estPremier est encore vrai, c’est un nombre premier
+    if ($estPremier) {
+        echo $i . "<br />";
+    }
+}
+?>
